e03ecacf33ca68efbae5ecc2f6051b3f8e9d7226
arshdeep singh <arshdeep4909@gmail.com>
2022-02-27 17:46:51 -0500

    5. styling AboutSection.

--- notes.md
+++ notes.md
@@ -1,1 +1,7 @@
 <!-- writing some random notes throughout the project in this file  -->
+
+# GlobalStyle
+
+we created a seperate file to create all the global styles
+for the file.
+imported in our APP and then defined it as an element in App()
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyled";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
       <h1>random </h1>
+      <GlobalStyle />
+      {/* importing file to apply global styles  */}
       <AboutUs />
     </div>
   );
